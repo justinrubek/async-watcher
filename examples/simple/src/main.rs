85515876715952addfc2ca789671da5eb6ef5b67
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-09-18 19:57:23 -0500

    docs(examples): greatly simplify the `simple` example
    This entirely removes the `async_debounce_watch` function in favor of
    using a channel to receive events. As a result the example is much more
    deserving of its name.

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,24 +1,15 @@
-use std::{path::Path, time::Duration};
-
 use async_watcher::{notify::RecursiveMode, AsyncDebouncer};
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // initialize the debouncer
+    let (mut debouncer, mut file_events) =
+        AsyncDebouncer::new_with_channel(Duration::from_secs(1), Some(Duration::from_secs(1)))
+            .await?;
+
+    // register the paths to be watched
     let paths = vec!["Cargo.toml", "Cargo.lock", "crates", "examples"];
-
-    async_debounce_watch(paths).await?;
-
-    Ok(())
-}
-
-pub async fn async_debounce_watch<P: AsRef<Path>>(
-    paths: Vec<P>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
-
-    let mut debouncer =
-        AsyncDebouncer::new(Duration::from_secs(1), Some(Duration::from_secs(1)), tx).await?;
-
     paths.iter().for_each(|p| {
         debouncer
             .watcher()
@@ -26,17 +17,9 @@ pub async fn async_debounce_watch<P: AsRef<Path>>(
             .unwrap();
     });
 
-    while let Some(event) = rx.recv().await {
-        match event {
-            Ok(events) => {
-                events.iter().for_each(|e| println!("event: {:?}", e));
-            }
-            Err(errors) => {
-                for error in errors {
-                    println!("error: {error:?}");
-                }
-            }
-        }
+    // wait for events
+    while let Some(event) = file_events.recv().await {
+        println!("event: {:?}", event);
     }
 
     Ok(())
