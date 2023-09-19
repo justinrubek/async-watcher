f6bf758cca54288a3f5f8b6b1896ace53de54b83
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-09-18 20:02:36 -0500

    docs(examples/rebuild): utilize AsyncDebouncer::new_with_channel

--- examples/rebuild/src/main.rs
+++ examples/rebuild/src/main.rs
@@ -1,24 +1,14 @@
-use std::{path::Path, time::Duration};
-
 use async_watcher::{notify::RecursiveMode, AsyncDebouncer};
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (mut debouncer, mut file_events) =
+        AsyncDebouncer::new_with_channel(Duration::from_secs(1), Some(Duration::from_secs(1)))
+            .await?;
+
+    // register the paths to be watched
     let paths = vec!["Cargo.toml", "Cargo.lock", "crates", "examples"];
-
-    async_debounce_watch(paths).await?;
-
-    Ok(())
-}
-
-pub async fn async_debounce_watch<P: AsRef<Path>>(
-    paths: Vec<P>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
-
-    let mut debouncer =
-        AsyncDebouncer::new(Duration::from_secs(1), Some(Duration::from_secs(1)), tx).await?;
-
     paths.iter().for_each(|p| {
         debouncer
             .watcher()
@@ -29,7 +19,7 @@ pub async fn async_debounce_watch<P: AsRef<Path>>(
     // keep track of the child process so we can kill it later
     let mut build_process: Option<tokio::process::Child> = None;
 
-    while let Some(event) = rx.recv().await {
+    while let Some(event) = file_events.recv().await {
         match event {
             Ok(_events) => {
                 // for this example we are triggering on any event, so we are not checking the info
