af5e5276c41631873761ab9052fbb0978a30e455
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-03-30 21:14:38 -0500

    feat(cli): add ability to skip command invocation on startup
    The `-s` or `--skip-launch-on-startup` flag has been added. By default,
    the command will be invoked right away when `awatch` is launched. If
    this behavior is not desired, then the flag will ensure it only spawns a
    child command after a file event has been received.

--- crates/cli/src/commands.rs
+++ crates/cli/src/commands.rs
@@ -4,4 +4,8 @@ pub(crate) struct Args {
     pub glob: String,
     pub command: String,
     pub args: Vec<String>,
+
+    #[arg(short, long)]
+    /// If set, the command will not be invoked until the first file change event is received.
+    pub skip_launch_on_startup: bool,
 }
